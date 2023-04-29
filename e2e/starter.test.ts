c53e0781c938137b09526101ebc88665a1e17efb
Bruna Chagas <brunabrk@gmail.com>
2023-04-29 16:19:21 +0200

    Organize tests and create one more

--- e2e/starter.test.ts
+++ e2e/starter.test.ts
@@ -15,12 +15,22 @@ describe('Example', () => {
   })
 
   it('should be able to play a meditation', async () => {
-    await element(by.id('ff171f80-5960-41e7-965c-1f9bcf31e02c')).tap()
+    await element(by.id('meditation-item')).atIndex(0).tap()
     await element(by.id('play-circle')).tap()
   })
 
-  it('should see the current streak, sessions and time changing', async () => {
+  it('should be able to manually create an entry and updates the sessions and time changing', async () => {
     await element(by.label('Stats')).tap()
-    await expect(element(by.id('currentStreak'))).toBeVisible()
+
+    const today: Date = new Date()
+    const date: string = ('0' + today.getDate()).slice(-2)
+    const timeMeditation: string = '33'
+    await element(by.text(date)).atIndex(0).tap()
+    await element(by.id('input')).typeText(timeMeditation)
+    await element(by.id('submit-btn')).tap()
+
+    await expect(element(by.id('current-streak'))).toHaveText('0 days')
+    await expect(element(by.id('total-sessions'))).toHaveText('1 session')
+    await expect(element(by.id('listened-stat'))).toHaveText(`${timeMeditation} minutes`)
   })
 })
