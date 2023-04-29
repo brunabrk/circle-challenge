f6d0bcad364cdf031512e7e05ec76f244b476eab
Bruna Chagas <brunabrk@gmail.com>
2023-04-29 20:47:11 +0200

    Apply page object model on the tests

--- e2e/meditation.test.ts
+++ e2e/meditation.test.ts
@@ -1,4 +1,6 @@
 import { device, element, by, expect } from 'detox'
+import { meditationScreen } from './screens/meditation'
+import { statsScreen } from './screens/stats'
 
 describe('Meditation test cases', () => {
   beforeAll(async () => {
@@ -13,20 +15,13 @@ describe('Meditation test cases', () => {
   })
 
   it('should be able to play a meditation and store its stats', async () => {
-    await element(by.id('meditation-item')).atIndex(0).tap()
-    await element(by.id('play-circle')).tap()
+    await meditationScreen().playMeditation()
+    await meditationScreen().finishMeditation()
 
-    while (await element(by.id('forward-10'))) {
-      try {
-        await element(by.id('forward-10')).tap()
-      } catch (error) {
-        break
-      }
-    }
     await element(by.id('skip')).tap()
     await element(by.label('Stats')).tap()
 
-    await expect(element(by.id('current-streak'))).toHaveText('1 day')
-    await expect(element(by.id('total-sessions'))).toHaveText('1 session')
+    await expect(statsScreen().elements.currentStreak()).toHaveText('1 day')
+    await expect(statsScreen().elements.totalSessions()).toHaveText('1 session')
   })
 })
--- e2e/stats.test.ts
+++ e2e/stats.test.ts
@@ -1,4 +1,6 @@
-import { device, element, by, expect } from 'detox'
+import { device, expect } from 'detox'
+import { statsScreen } from './screens/stats'
+import { meditationScreen } from './screens/meditation'
 
 describe('Stats test cases', () => {
   beforeAll(async () => {
@@ -13,17 +15,14 @@ describe('Stats test cases', () => {
   })
 
   it('should be able to manually create an entry and updates the sessions and time changing', async () => {
-    await element(by.label('Stats')).tap()
+    await meditationScreen().elements.stats().tap()
+    await statsScreen().selectDate()
 
-    const today: Date = new Date()
-    const date: string = ('0' + today.getDate()).slice(-2)
     const timeMeditation: string = '33'
-    await element(by.text(date)).atIndex(0).tap()
-    await element(by.id('input')).typeText(timeMeditation)
-    await element(by.id('submit-btn')).tap()
+    await statsScreen().fillMeditation(timeMeditation)
 
-    await expect(element(by.id('current-streak'))).toHaveText('0 days')
-    await expect(element(by.id('total-sessions'))).toHaveText('1 session')
-    await expect(element(by.id('listened-stat'))).toHaveText(`${timeMeditation} minutes`)
+    await expect(statsScreen().elements.currentStreak()).toHaveText('0 days')
+    await expect(statsScreen().elements.totalSessions()).toHaveText('1 session')
+    await expect(statsScreen().elements.listenedStat()).toHaveText(`${timeMeditation} minutes`)
   })
 })
