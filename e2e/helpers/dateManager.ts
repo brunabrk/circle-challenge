e7aac33dfc2ea1395a9499827a0aa0927bf785c5
Bruna Chagas <brunabrk@gmail.com>
2023-05-02 10:23:21 +0200

    Fix date to not have 0 at the begining

--- e2e/helpers/dateManager.ts
+++ e2e/helpers/dateManager.ts
@@ -1,6 +1,10 @@
 const getYesterdayDate = () => {
   const today: Date = new Date()
-  return new Date(today.setDate(today.getDate() - 1)).toString().slice(8, 10)
+  let yesterday: string = new Date(today.setDate(today.getDate() - 1)).toString().slice(8, 10)
+  if (yesterday.charAt(0) === '0') {
+    yesterday = yesterday.replace('0', '')
+  }
+  return yesterday
 }
 
 export { getYesterdayDate }
